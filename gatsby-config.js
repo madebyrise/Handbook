3f32d0524e18e048cecaa867b1fee51d59f7a888
Matt Collins <mailmattcollins@gmail.com>
2020-03-16 11:44:50 +0000

    Add gatsby-remark-autolink-headers.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-autolink-headers`],
+      },
+    },
     `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
